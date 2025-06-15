25199877b5f798c718ca8a441bf20756f25e3e0f
DanielGaming0 <carlosdanielmarinho988@gmail.com>
2025-06-15 15:53:48 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function adicionarServidor() {
-  window.location.href = 'minecraft://?addExternalServer=Dancas Mal Server|bp66-de1.creepercloud.io:55769';
+  window.location.href = 'minecraft://?addExternalServer=Dancas%20Mal|bp66-de1.creepercloud.io:55769';
 }
